2ce298d64ead5b1c07049b3d68c2bb437a6db9ef
ozhiron <43087781+ozhiron@users.noreply.github.com>
2018-12-08 19:20:19 +0300

    Update Unicode.java

--- Unicode.java
+++ Unicode.java
@@ -1,6 +1,6 @@
 public class Unicode {
-	public static void main(String[] args){
-		for(char i = 9398; i != 10178; i++){
+	public static void main(String[] args) {
+		for(char i = 9398; i != 10178; i++) {
 			System.out.print(i + " ");
 		}
 	}
