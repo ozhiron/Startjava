f912d53303e72d72effddcb53cdfb05d916b98e0
ozhiron <kro1998@yandex.ru>
2018-12-08 17:15:15 +0300

    Calcualtor

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator{
+	public static void main(String[] args) {
+	int y = 8;
+	int x = 2;
+	char mathOperation = '^';
+
+	if (mathOperation == '+') {
+		System.out.println(y + x);
+		} else if (mathOperation == '-') {
+			System.out.println(y - x);
+		}else if (mathOperation == '*') {
+			System.out.println(y * x);
+		}else if (mathOperation == '/') {
+			System.out.println(y / x);
+		}else if (mathOperation == '%') {
+			System.out.println(y % x);
+		}else if (mathOperation == '^') {
+				int result = 1;
+				for (int i = 1; i <= y; i++) {
+				result *= x;
+			}
+				System.out.println(result); {
+				}
+			}
+		}
+		
+}
